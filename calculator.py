099bca4882b485e5c2e20afd0f3baac38835a9a9
Harry Kim <harryk9395@gmail.com>
2022-08-30 20:37:46 -0700

    Made it so asking for inputs is only appears once in code depending on operation chosen

--- calculator.py
+++ calculator.py
@@ -1,24 +1,28 @@
 again = "Yes"
 while(again != "No"):
     operation = input("Enter an operation (addition, subtraction, multiplication, divison, factorial, fibonacci): ")
-    if operation == "addition":
+
+    if operation == "addition" or "subtraction" or "multiplication" or "division":
         num1 = int(input("Enter a number: "))
         num2 = int(input("Enter second number: "))
-        result = str(num1 + num2)
-        print("The result is: " + result)
-    if operation == "subtraction":
-        num1 = int(input("Enter a number: "))
-        num2 = int(input("Enter second number: "))
-        result = str(num1 - num2)
-        print("The result is: " + result)
-    if operation == "multiplication":
-        num1 = int(input("Enter a number: "))
-        num2 = int(input("Enter second number: "))
-        result = str(num1 * num2)
-        print("The result is: " + result)  
-    if operation == "division":
-        num1 = int(input("Enter a number: "))
-        num2 = int(input("Enter second number: "))
-        result = (num1 / num2)
-        print("The result is: " + str(round(result, 3)))
+
+        if operation == "addition":
+            result = str(num1 + num2)
+            print("The result is: " + result)
+        if operation == "subtraction":
+            result = str(num1 - num2)
+            print("The result is: " + result)
+        if operation == "multiplication":
+            result = str(num1 * num2)
+            print("The result is: " + result)  
+        if operation == "division":
+            result = (num1 / num2)
+            print("The result is: " + str(round(result, 3)))
+
+    if operation == "factorial" or "fibonacci":
+        if operation == "factorial":
+            num1 = int(input("Enter a number: "))
+
+
+
     again = input("Enter [No] to stop: ")
