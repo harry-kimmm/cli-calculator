23b49b3c64c0b73653e51d2e13044fa83263e438
Harry Kim <harryk9395@gmail.com>
2022-08-30 20:03:39 -0700

    Add addition, subtraction, multiplication

---
+++ calculator.py
@@ -0,0 +1,20 @@
+again = "Yes"
+while(again != "No"):
+    operation = input("Enter an operation (addition, subtraction, multiplication, divison, factorial, fibonacci): ")
+    if operation == "addition":
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter second number: "))
+        result = str(num1 + num2)
+        print("The result is: " + result)
+    if operation == "subtraction":
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter second number: "))
+        result = str(num1 - num2)
+        print("The result is: " + result)
+    if operation == "multiplication":
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter second number: "))
+        result = str(num1 * num2)
+        print("The result is: " + result)    
+    again = input("Enter [No] to stop: ")
+    
