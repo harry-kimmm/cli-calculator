37155124f9f6820c4a184a894b78b5da5d00b8f9
Harry Kim <harryk9395@gmail.com>
2022-08-30 22:40:50 -0700

    Remove previous change because it made the factorial one wonky and i dont wanna deal with it at the moment

--- calculator.py
+++ calculator.py
@@ -2,27 +2,32 @@ again = "Yes"
 while(again != "No"):
     operation = input("Enter an operation (addition, subtraction, multiplication, divison, factorial, fibonacci): ")
 
-    if operation == "addition" or "subtraction" or "multiplication" or "division":
+    if operation == "addition":
         num1 = int(input("Enter a number: "))
         num2 = int(input("Enter second number: "))
-
-        if operation == "addition":
-            result = str(num1 + num2)
-            print("The result is: " + result)
-        if operation == "subtraction":
-            result = str(num1 - num2)
-            print("The result is: " + result)
-        if operation == "multiplication":
-            result = str(num1 * num2)
-            print("The result is: " + result)  
-        if operation == "division":
-            result = (num1 / num2)
-            print("The result is: " + str(round(result, 3)))
-
-    if operation == "factorial" or "fibonacci":
-        if operation == "factorial":
+        result = str(num1 + num2)
+        print("The result is: " + result)
+    if operation == "subtraction":
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter second number: "))
+        result = str(num1 - num2)
+        print("The result is: " + result)
+    if operation == "multiplication":
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter second number: "))
+        result = str(num1 * num2)
+        print("The result is: " + result)  
+    if operation == "division":
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter second number: "))
+        result = (num1 / num2)
+        print("The result is: " + str(round(result, 3)))
+        
+    if operation == "factorial": 
             num1 = int(input("Enter a number: "))
-
-
-
+            for i in range(num1):
+                nextnum = (num1 - 1)
+                num1 = num1 * nextnum
+            result = num1
+            print("The result is: " + str(result))
     again = input("Enter [No] to stop: ")
