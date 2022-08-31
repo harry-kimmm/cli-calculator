dabe99ff80b0972e16db53505c4cc8c91df48269
Harry Kim <harryk9395@gmail.com>
2022-08-30 20:09:30 -0700

    Add division rounded to 3 decimal places

--- calculator.py
+++ calculator.py
@@ -15,6 +15,10 @@ while(again != "No"):
         num1 = int(input("Enter a number: "))
         num2 = int(input("Enter second number: "))
         result = str(num1 * num2)
-        print("The result is: " + result)    
+        print("The result is: " + result)  
+    if operation == "division":
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter second number: "))
+        result = (num1 / num2)
+        print("The result is: " + str(round(result, 3)))
     again = input("Enter [No] to stop: ")
-    
